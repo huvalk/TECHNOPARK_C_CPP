102a2356753a707946dc1c1c616a3db3c1d76e3e
huvalk <akiraq@yandex.ru>
2019-10-24 18:57:27 +0300

    formating

--- dyn_lib/d_user_messages.c
+++ dyn_lib/d_user_messages.c
@@ -10,23 +10,23 @@ extern size_t SIZE_MESSAGE;
 extern size_t SIZE_DICT;
 
 
-struct export_vtable {
-    Dict* (*find_messages)(size_t* const message_count, const Message* const messages, char* const user, const Date* const period);
+struct exportVtable {
+    Dict *(*findMessages)(size_t *const message_count, const Message *const messages, char *const user,
+                          const Date *const period);
 };
 
-Dict* find_messages( size_t* const message_count, const Message* const messages,
-                     char* const user, const Date* const period )
-{
+Dict *findMessages(size_t *const message_count, const Message *const messages,
+                   char *const user, const Date *const period) {
     size_t cap = DEF_ARR_SIZE, size = 0;
-    Dict* dict = calloc(cap, SIZE_DICT);
-    if(dict == NULL)
+    Dict *dict = calloc(cap, SIZE_DICT);
+    if (dict == NULL)
         return NULL;
-    for(size_t i = 0; i < *message_count; i++) {
-        int8_t flag = in_recievers(user, (messages + i));
-        if(  flag == 1 && in_period(period, (messages + i)) ) {
-            if( size >= cap ) {
-                Dict* nw_dict = malloc( cap * SIZE_DICT * 2 );
-                if(nw_dict == NULL)
+    for (size_t i = 0; i < *message_count; i++) {
+        int8_t flag = inRecievers(user, (messages + i));
+        if (flag == 1 && inPeriod(period, (messages + i))) {
+            if (size >= cap) {
+                Dict *nw_dict = malloc(cap * SIZE_DICT * 2);
+                if (nw_dict == NULL)
                     return NULL;
                 memcpy(nw_dict, dict, cap * SIZE_DICT);
 
@@ -35,18 +35,18 @@ Dict* find_messages( size_t* const message_count, const Message* const messages,
                 dict = nw_dict;
             }
 
-            to_dict_elem((messages + i), (dict + size));
+            toDictElem((messages + i), (dict + size));
             size++;
         } else if (flag == -1) {
             return NULL;
         }
     }
 
-    if ( Sort(dict, size, cmp_dict_men) == false )
+    if (sort(dict, size, cmpDictMen) == false)
         return NULL;
 
     *message_count = size;
     return dict;
 }
 
-struct export_vtable exports = {find_messages};
+struct exportVtable exports = {findMessages};
--- dyn_lib/d_user_messages.h
+++ dyn_lib/d_user_messages.h
@@ -1,8 +1,9 @@
 #ifndef IZ2_D_USER_MESSAGES_H
 #define IZ2_D_USER_MESSAGES_H
 
-#include "../mes/message.h"
+#include "../common/mes_sort.h"
 
-Dict* find_messages(size_t* const message_count, const Message* const messages, char* const user, const Date* const period);
+Dict *findMessages(size_t *const message_count, const Message *const messages, char *const user,
+                   const Date *const period);
 
 #endif //IZ2_D_USER_MESSAGES_H
